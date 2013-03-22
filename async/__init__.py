0ac63ca298558b14087dabbf89f9e04c3ef4e175
Klaas Cuvelier <klaas@showpad.com>
2013-03-22 18:21:15 +0100

    thread > threading

--- async/__init__.py
+++ async/__init__.py
@@ -9,15 +9,15 @@ def _init_atexit():
 	"""Setup an at-exit job to be sure our workers are shutdown correctly before
 	the interpreter quits"""
 	import atexit
-	import thread
+	import threading
 	atexit.register(thread.do_terminate_threads)
-	
+
 def _init_signals():
 	"""Assure we shutdown our threads correctly when being interrupted"""
 	import signal
-	import thread
+	import threading
 	import sys
-	
+
 	prev_handler = signal.getsignal(signal.SIGINT)
 	def thread_interrupt_handler(signum, frame):
 		thread.do_terminate_threads()
